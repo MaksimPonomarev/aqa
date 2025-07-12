4cc37945e8cb527886ae3806701e0a94d5ea4a7d
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-07-12 16:25:30 +0300

    + задание

--- mounth1/block1.py
+++ mounth1/block1.py
@@ -1,6 +1,17 @@
-name = 'Максим'
-age = 20
-sleep_hours = 8.2
-activity = True
 
-print(f"Имя: {name}\nВозраст: {age}\nСпит в среднем: {sleep_hours}\nАктивен: {activity}")
+
+# name = 'Максим'
+# age = 20
+# sleep_hours = 8.2
+# activity = True
+
+#print(f"Имя: {name}\nВозраст: {age}\nСпит в среднем: {sleep_hours}\nАктивен: {activity}")
+
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+sleep_hours = float(input("Enter your sleep hours: "))
+activity = input("Enter your activity (True/False): ").strip().lower() == "true"
+
+print(f"Имя: {name}\nВозраст: {age}\nСпит в среднем: {sleep_hours}\nАктивен:{'Да' if activity else 'Нет'}")
