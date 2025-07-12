3bba9d4147a7320b4200d4771ce93ed73b103e55
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-07-12 17:26:05 +0300

    блок 2 задания

--- mounth1/block2.py
+++ mounth1/block2.py
@@ -1,6 +1,10 @@
 my_age = int(input("Enter your age: "))
 friend_age = int(input("Enter your friend age: "))
-sleep_days = int(input("Enter your sleep days per year: "))
+sleep_hours = int(input("Enter your sleep hours per day: "))
+
+if my_age < 0 or friend_age < 0 or sleep_hours < 0:
+    print("Данные не могут быть отрицательными")
+    exit()
 
 who_oldest = str()
 age_difference = abs(my_age - friend_age)
@@ -14,5 +18,5 @@ else:
 
 print(who_oldest)
 print(f"Ваша разнциа в возрасте составляет: {age_difference} (год(а)/лет)")
-print(f"За этот год ты поспал: {sleep_days*365} часов")
+print(f"За этот год ты поспал: {sleep_hours*365} часов")
 print(f"Оба ли старше 18 лет? {'Да' if my_age >= 18 and friend_age >= 18 else 'Нет'}")
