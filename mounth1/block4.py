aed002ae46bd0ccaba07be86a9c579c76bece6ea
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-07-13 23:13:30 +0300

    Доделал все задачи

--- mounth1/block4.py
+++ mounth1/block4.py
@@ -1,19 +1,20 @@
-# mats = ["blyat", "syka", "naxuy"]
+# # mats = ["blyat", "syka", "naxuy"]
+# #
+# # for i in range(len(mats)):
+# #     print(f"Индекс в списке: {i}, Маты: {mats[i]}")
+# #
+# # for index, mats in enumerate(mats):
+# #     print(f"{index}. {mats}")
+# #
+# # squares = [x for x in range(90)]
+# # print(squares)
 #
-# for i in range(len(mats)):
-#     print(f"Индекс в списке: {i}, Маты: {mats[i]}")
 #
-# for index, mats in enumerate(mats):
-#     print(f"{index}. {mats}")
-#
-# squares = [x for x in range(90)]
-# print(squares)
-
-
 list_mov = []
 count = 0
 max_len_raw = 0
 max_word = []
+num = 0
 
 while True:
     moves = input("Введи любимый фильм: ")
@@ -36,9 +37,29 @@ for index, move in enumerate(reversed(list_mov)):
 
 print("Самое длинное слово:", *max_word)
 
-answer = input("Хочешь найти какой то фильм, который ты присал? ")
+
+for i in list_mov:
+    num = len(i) + num
+
+avg = num / len(list_mov)
+print(f"Средняя длина названия: {round(avg, 2)}")
+
+answer = input("Хочешь найти какой то фильм, который ты присал? ").lower()
 if answer == "да":
     film = input("какой фильм хочешь найти? ")
     for index, mov in enumerate(list_mov):
-        if mov == film:
+        if mov.lower() == film.lower():
             print(f"Вот твой фильм: {film}, он под номером {index + 1}")
+
+
+
+change_film = input("Хочешь поменять какой то фильм из списка? ").lower()
+if change_film == "да":
+    num_film = int(input("какой фильм хочешь менять? Укажи номер "))
+    for index, mov in enumerate(list_mov):
+        if index == num_film:
+            print("зашел во вложенный цикл")
+            list_mov.pop(num_film)
+            add_film = input("Какой фильм хочешь поставить на это место?")
+            list_mov.insert(num_film - 1, add_film)
+print(f"Обновленный список фильмов: {list_mov}")
