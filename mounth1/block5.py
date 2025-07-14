7029f7ba7bd561d56313b84d3db4d535a90e7300
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-07-15 01:54:09 +0300

    Начал задачи

---
+++ mounth1/block5.py
@@ -0,0 +1,42 @@
+# def hello():
+#     print("Хеллоу мир манера крутит мир")
+# print(hello())
+#
+# def square(x):
+#     return x**2
+#
+# print(square(8))
+#
+# def greet(name, role="qa engineer"):
+#     print(f"Привет, {name}! Ты - {role}")
+#
+# greet("Максим")
+# greet("Максим", "qa")
+
+# def power(x, p=2):
+#     return x**p
+#
+# print(power(5,10))
+# print(power(5))
+
+# def list_status(nums):
+#     """множественный return, кортеж, len, sum"""
+#     tuple(nums)
+#     return nums
+# list_a = [1,2,3,4,5,5,4,3,1,231,3,1,3,13,42,3,431,32,1,1,11,11,1]
+# print(list_status(list_a))
+
+#
+# def flex_sum(*args, **kwargs):
+#     sum_num = sum(args)
+#     all_sum = sum_num + sum(kwargs)
+#     return all_sum
+#
+# print(flex_sum(1,2,3,1.3,3))
+
+
+def apply(func,iterable):
+    pass
+
+def make_counter():
+    pass
