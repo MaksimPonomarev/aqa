2f2bf908a1e719a43ee69a1e3636a4d59e54469b
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-07-18 01:40:35 +0300

    Добавил все задачи кроме дополнительных

--- mounth1/block5.py
+++ mounth1/block5.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 # def hello():
 #     print("Хеллоу мир манера крутит мир")
 # print(hello())
@@ -28,15 +30,106 @@
 
 #
 # def flex_sum(*args, **kwargs):
-#     sum_num = sum(args)
-#     all_sum = sum_num + sum(kwargs)
-#     return all_sum
+#     sum_args = sum(args)
+#     sum_kwargs = 0
+#     for keys in kwargs:
+#         value = kwargs.get(keys)
+#         if isinstance(value,(int,float)):
+#             sum_kwargs = sum_kwargs + value
+#         else:
+#             return "Ты зачем сюда строку пихнул? ТОЛЬКО ЦИФРЫ"
 #
-# print(flex_sum(1,2,3,1.3,3))
+#     return sum_args + sum_kwargs
+#
+# print(flex_sum(22,92, a=9,b=456,c=5,d="hfyhv"))
 
 
-def apply(func,iterable):
-    pass
 
-def make_counter():
-    pass
+# def apply(func, *iterable):
+#     new_list = []
+#     for i in iterable:
+#         new_list.append(func(i))
+#     return new_list
+#
+# print(apply(lambda x: x**2, 3,4,5,6,7))
+
+
+
+# sort_by_length = lambda words, x:sorted(words, key=None, reverse = x)
+# a = ['efwfewf','12erfwsvc','1232222222222222222222222222222222222222222dfs','1','12','123']
+# x = True
+# print(sort_by_length(a,x))
+#
+# p.s "в этой задаче не понял, что ты от меня хочешь, решил так."
+
+
+
+# def make_counter():
+#     count = 0
+#
+#     def counter():
+#         nonlocal count
+#         count += 1
+#
+#     counter()
+#     counter()
+#     counter()
+#     counter()
+#     counter()
+#     return count
+#
+# print(make_counter())
+
+#
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+#     else:
+#         list_num = []
+#         for i in range(n):
+#             list_num.append(i+1)
+#         return reduce((lambda x,y: x*y), list_num)
+
+
+# print(factorial(5))
+#
+# def factorial_v_2(n):
+#     if n <= 1:
+#         return 1
+#     return n * factorial_v_2(n-1)
+#
+# print(factorial_v_2(5))
+#
+def fibo(n, a=0,b=1):
+    while True:
+        c = a + b
+        yield a+b
+        a = b
+        b = c
+        n -= 1
+        if n == 0:
+            break
+
+n = int(input("До какого чилса? "))
+for i in fibo(n):
+    print(i)
+
+
+
+
+
+# def iii():
+#     i = 0
+#     while True:
+#         yield i * 2
+#         i += 1
+#
+#
+#
+# count = 0
+# for i in iii():
+#     if count >= 15:
+#         break
+#     print(i)
+#     count += 1
+#
