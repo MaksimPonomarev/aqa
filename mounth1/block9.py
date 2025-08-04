f2b32cc73e74aa7434fc6898a1156436b2f21dcd
Maksim Ponomarev <ponomarevmaksim989@gmail.com>
2025-08-05 01:30:22 +0300

    словари

--- mounth1/block9.py
+++ mounth1/block9.py
@@ -112,10 +112,125 @@
 # for i in a:
 #     b.append(i.upper())
 # print(b)
+#
+# a = [1, -2, 3, -4, 5]
+# b = []
+# for i in a:
+#     if i >= 0:
+#         b.append(i)
+# print(b)
 
-a = [1, -2, 3, -4, 5]
-b = []
-for i in a:
-    if i >= 0:
-        b.append(i)
-print(b)
+
+# matrix = [[1, 2], [3, 4]]
+# flattened = [x for row in matrix for x in row]
+# print(flattened)
+
+
+
+
+#Задачи на списковые включения (List Comprehension)
+# a = [1,2,3,4]
+# b = [x*2 for x in a]
+# print(b)
+#
+# a = ["python", "java", "c++"]
+# b = [x.upper() for x in a]
+# print(b)
+#
+#
+# a = [-4, 3, -2, 0, 1]
+# b = [x for x in a if x>0]
+# print(b)
+#
+# a = [1, 2, 3, 4, 5]
+# b = [x**2 for x in a if x%2==0]
+# print(b)
+#
+# a = [1, 2, 3, 4, 5]
+# b = ["чет" if x%2==0 else "нечет" for x in a ]
+# print(b)
+#
+# a = [[1, 2], [3, 4], [5, 6]]
+# b = [x for row in a for x in row]
+# print(b)
+#
+# a = (1,2,3,4)
+# print(len(a))
+# print(a[1])
+#
+# tup = (10,20,30)
+# a,b,c = tup
+# print(a,b,c)
+#
+# a = (1,2,3)
+# try:
+#     a[0] = 99
+# except TypeError as e:
+#     print(e)
+# print(a)
+#
+# a = (7,)
+# print(type(a))
+#
+# a = (1,2,2,3,2)
+# print(a.count(2))
+# print(a.index(2))
+#
+# a = [("a", 1), ("b", 2), ("c", 3)]
+# for letter, number in a:
+#     print("Буква:", letter)
+#     print("Число:", number)
+
+
+# a = {
+#     (0, 0): "начало",
+#     (1, 2): "точка A"
+# }
+#
+# text = a[(1,2)]
+# print(text)
+#
+# for key in a.keys():
+#     x,y=key
+#     if x == 1 and y == 2:
+#         print(a[key])
+
+
+# data = ((1, 2), (3, 4), (5, 6))
+# count=0
+# for elem in data:
+#     for i in elem:
+#         count+=i
+#
+# print(count)
+
+
+
+#Задачи на словари
+person = {
+    "name": "Maksim",
+    "age": 20,
+    "city": "Moscow"
+}
+print(person["city"])
+
+user = {
+    "login": "user123"
+}
+
+user["email"] = "test@example.com"
+user["login"] = "admin"
+
+data = {
+    "x": 1,
+    "y": 2
+}
+print(data.get("z", "Нет данных"))
+
+scores = {
+    "Анна": 90,
+    "Борис": 80,
+    "Вера": 95
+}
+for key, values in scores.items():
+    print(f"{key} получил(а) {values}")
